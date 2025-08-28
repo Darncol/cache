326c96156184a6ea18525c4a3f14e9f0cedca79b
Darncol <60666804+Darncol@users.noreply.github.com>
2025-08-28 20:41:00 +0300

    Add mutex for thread-safe cache operations

--- cache.go
+++ cache.go
@@ -1,19 +1,28 @@
 package cache
 
+import "sync"
+
 type Cache struct {
 	cache map[string]interface{}
+	mu    *sync.RWMutex
 }
 
 func (c *Cache) Set(key string, value interface{}) {
+	c.mu.Lock()
 	c.cache[key] = value
+	c.mu.Unlock()
 }
 
 func (c *Cache) Get(key string) interface{} {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
 	return c.cache[key]
 }
 
 func (c *Cache) Delete(key string) {
+	c.mu.Lock()
 	delete(c.cache, key)
+	c.mu.Unlock()
 }
 
 func New() *Cache {
