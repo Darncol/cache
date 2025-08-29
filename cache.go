7faeec77a949b385a574803ee44a8cdfd46c0764
Darncol <60666804+Darncol@users.noreply.github.com>
2025-08-29 15:54:17 +0300

    Refactor cache implementation to support expiration with uninstaller goroutine

--- cache.go
+++ cache.go
@@ -1,15 +1,43 @@
 package cache
 
-import "sync"
+import (
+	"sync"
+	"time"
+)
 
 type Cache struct {
-	cache map[string]interface{}
-	mu    *sync.RWMutex
+	cache    map[string]interface{}
+	timeLeft map[string]chan struct{}
+	mu       *sync.RWMutex
 }
 
-func (c *Cache) Set(key string, value interface{}) {
+func (c *Cache) uninstaller(timeLeft time.Duration, key string, timer chan struct{}) {
+	for {
+		select {
+		case <-timer:
+			return
+		case <-time.After(timeLeft):
+			c.Delete(key)
+			return
+		}
+	}
+}
+
+func (c *Cache) Set(key string, value interface{}, duration time.Duration) {
 	c.mu.Lock()
+
+	oldTimer, ok := c.timeLeft[key]
+	if ok {
+		oldTimer <- struct{}{}
+	}
+
+	timer := make(chan struct{})
+
+	c.timeLeft[key] = timer
 	c.cache[key] = value
+
+	go c.uninstaller(duration, key, timer)
+
 	c.mu.Unlock()
 }
 
@@ -21,12 +49,23 @@ func (c *Cache) Get(key string) interface{} {
 
 func (c *Cache) Delete(key string) {
 	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	timer, ok := c.timeLeft[key]
+	if ok {
+		timer <- struct{}{}
+	}
+
+	close(timer)
+
 	delete(c.cache, key)
-	c.mu.Unlock()
+	delete(c.timeLeft, key)
 }
 
 func New() *Cache {
 	return &Cache{
-		cache: make(map[string]interface{}),
+		cache:    make(map[string]interface{}),
+		timeLeft: make(map[string]chan struct{}),
+		mu:       &sync.RWMutex{},
 	}
 }
